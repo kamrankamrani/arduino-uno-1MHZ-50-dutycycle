2529ea479aad89e4751200d8f79dbb6ecced01b2
kamrankamranifard <50171202+kamrankamrani@users.noreply.github.com>
2020-01-08 13:39:29 +0330

    Add files via upload

---
+++ arduino_1MHZ.ino
@@ -0,0 +1,21 @@
+const uint8_t TIMER2_OUTPUT = 3;  // OC2B
+const uint8_t PERIOD = 16;      // 16 MHZ / 16 = 1 MHZ
+
+void setup()
+{
+    pinMode(TIMER2_OUTPUT, OUTPUT);
+    TCCR2B = 0;           // stop timer
+    TCNT2  = 0;           // reset timer
+    TCCR2A = _BV(COM2B1)  // non-inverting PWM on OC2B
+           | _BV(WGM20)   // fast PWM mode, TOP = OCR2A
+           | _BV(WGM21);  
+    TCCR2B = _BV(WGM22);  
+    OCR2A = PERIOD - 1;
+    OCR2B = PERIOD/2 - 1;
+}
+
+
+void loop(){
+  TCCR2B |= _BV(CS20); 
+}
+
---
+++ readme.md
@@ -0,0 +1,4 @@
+# ARDUINO UNO pulse generator  
+this example makes 1 mhz and 50% dutycycle pulse on the pin 3  
+if you want to change the frequency just change the 'PERIOD' variable divider  
+frequency = 16 MHZ / PERIOD  
